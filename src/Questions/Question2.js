73317926bacd8b92adb038fe7d3b46cb267c4d1f
Jongminfire <51112542+Jongminfire@users.noreply.github.com>
2021-05-13 22:36:59 +0900

    fix: fix import error

--- src/Questions/Question2.js
+++ src/Questions/Question2.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import styled, { css } from "styled-components";
 import ArrowForwardIosIcon from "@material-ui/icons/ArrowForwardIos";
 
