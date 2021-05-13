d16b849c679f69a9270db20fa38bb00a33d91825
Dongsik Ga <ret0422@gmail.com>
2021-05-13 15:44:36 +0900

    Modify: Welcome Page Design

--- src/Pages/WelcomePage.js
+++ src/Pages/WelcomePage.js
@@ -3,23 +3,30 @@ import { useHistory } from 'react-router-dom';
 import { useMediaQuery } from 'react-responsive';
 import styled, { css } from 'styled-components';
 import WaveEffect from '../Functions/WaveEffect';
+import './WelcomePage.css';
 
 const Background = styled.div`
   width: 100%;
   height: 100vh;
+  align-items: center;
+  justify-content: center;
+  display: flex;
+  flex-direction: column;
 `;
 
 const Contents = styled.div`
-  position: absolute;
   display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: #d4f2f6;
-  width: 40%;
-  height: 90%;
-  border-radius: 15px;
-  right: 30%;
   top: 5%;
+  min-width: 300px;
+  max-width: 500px;
+  width: 50vw;
+  height: 100vh;
+  background-color: #d4f2f6;
+  margin-top: 5vh;
+  border-radius: 10px 10px 0px 0px;
+  opacity: 0.75;
+  padding-left: 5vw;
+  padding-right: 5vw;
 `;
 
 const WelcomePage = (props) => {
@@ -28,9 +35,28 @@ const WelcomePage = (props) => {
     <Background>
       <Contents>
         <div>
-          PC 웰컴페이지 입니다.
-          <br />
-          <button onClick={onClickStart}>테스트 시작하기</button>
+          <div className="Title-font">음주가無</div>
+          <div className="Main-font">
+            Lorem ipsum dolor sot ametaaaaa, consectetur adipiscing elit ut
+            aliqualm
+          </div>
+          <div class="Button-div">
+            <button onClick={onClickStart} className="Button-start">
+              테스트 시작하기
+            </button>
+          </div>
+          <div className="Share-it-font">Share it! </div>
+          <div class="Button-share-div">
+            <div>
+              <button onClick={onClickStart} className="Button-share"></button>
+            </div>
+            <div>
+              <button onClick={onClickStart} className="Button-share"></button>
+            </div>
+            <div>
+              <button onClick={onClickStart} className="Button-share"></button>
+            </div>
+          </div>
         </div>
       </Contents>
       <WaveEffect />
