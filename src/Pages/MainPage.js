110059e4db2086a660b8b03ca9aabff71711048c
Jongminfire <51112542+Jongminfire@users.noreply.github.com>
2021-05-13 22:37:38 +0900

    fix: 불필요한 컨텐츠 제거 (score, 끝내기 버튼)

--- src/Pages/MainPage.js
+++ src/Pages/MainPage.js
@@ -53,26 +53,28 @@ const MainPage = (props) => {
 	const { onClickFinish, showQuestion, score, setCards, setAnswers } = props;
 	const [popped, setPopped] = useState(false);
 	const [selectedCard, setSelectedCard] = useState(null);
-	const testcards=[
-		{ id:0, bg: "linear-gradient(153.55deg, #879AF2 9.48%, #D3208B 48.25%, #FDA000 85.78%)" },
-		{ id:1, bg: "#D3208B" },
-		{ id:2, bg: "linear-gradient(to right, #00c6ff, #0072ff)" },
-		{ id:3, bg: "linear-gradient(to right, #780206, #061161)" },
-		{ id:4, bg: "linear-gradient(to right, #f0c27b, #4b1248)" }
-	]
+	const testcards = [
+		{ id: 0, bg: "linear-gradient(153.55deg, #879AF2 9.48%, #D3208B 48.25%, #FDA000 85.78%)" },
+		{ id: 1, bg: "#D3208B" },
+		{ id: 2, bg: "linear-gradient(to right, #00c6ff, #0072ff)" },
+		{ id: 3, bg: "linear-gradient(to right, #780206, #061161)" },
+		{ id: 4, bg: "linear-gradient(to right, #f0c27b, #4b1248)" },
+	];
 	return (
 		<Background>
 			<OtherBackground></OtherBackground>
-			<Contents>
-				{showQuestion()}
-				<br />
-				score: {score}
-				<br />
-				<button onClick={onClickFinish}>테스트 끝내기</button>
-			</Contents>
-			<CardDrawer clckevent={(card)=>{setPopped(true);setSelectedCard(card)}} cards={testcards}>sans here</CardDrawer>
+			<Contents>{showQuestion()}</Contents>
+			<CardDrawer
+				clckevent={(card) => {
+					setPopped(true);
+					setSelectedCard(card);
+				}}
+				cards={testcards}
+			>
+				sans here
+			</CardDrawer>
 			{/* <WaveEffect/> */}
-			<CardPopup popped={popped} setPopped={setPopped} card={selectedCard}/>
+			<CardPopup popped={popped} setPopped={setPopped} card={selectedCard} />
 		</Background>
 	);
 };
