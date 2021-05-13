d1f042ee48ca391f1b4748d050d85d28f4711a7b
Jongminfire <51112542+Jongminfire@users.noreply.github.com>
2021-05-14 02:50:50 +0900

    feat: Mobile 설문 페이지 애니메이션 추가

--- src/Mobile/MainPageMobile.js
+++ src/Mobile/MainPageMobile.js
@@ -1,14 +1,17 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
 import { useMediaQuery } from "react-responsive";
+import { SwitchTransition, CSSTransition } from "react-transition-group";
+import "./QuestionsMobile.css";
 
 const MainPageMobile = (props) => {
-	const { showQuestion, score } = props;
+	const { showQuestion, score, count } = props;
 	return (
-		<div>
-			{showQuestion()}
-			<br />
-		</div>
+		<SwitchTransition>
+			<CSSTransition key={count} addEndListener={(node, done) => node.addEventListener("transitionend", done, false)} classNames="fade">
+				{showQuestion()}
+			</CSSTransition>
+		</SwitchTransition>
 	);
 };
 
