f4026d148fae3a86c6eeaa056c69c01cd7ba9e2f
zensen6 <junhee1469@gmail.com>
2021-05-13 15:44:56 +0900

    renewed wave

--- src/Functions/WaveEffect.js
+++ src/Functions/WaveEffect.js
@@ -4,28 +4,12 @@ import midwave from '../image/wave-mid.png';
 import botwave from '../image/wave-bot.png';
 
 function WaveEffect() {
-  return (
-    <div class="waveWrapper waveAnimation">
-      <div class="waveWrapperInner bgTop">
-        <div
-          class="wave waveTop"
-          style={{ backgroundImage: `url(${topwave})`, opacity: 0.5 }}
-        ></div>
-      </div>
-      <div class="waveWrapperInner bgMiddle">
-        <div
-          class="wave waveMiddle"
-          style={{ backgroundImage: `url(${midwave})`, opacity: 0.5 }}
-        ></div>
-      </div>
-      <div class="waveWrapperInner bgBottom">
-        <div
-          class="wave waveBottom"
-          style={{ backgroundImage: `url(${botwave})`, opacity: 0.5 }}
-        ></div>
-      </div>
-    </div>
-  );
+	return (
+		<div class="ocean">
+			<div class="wave" />
+			<div class="wave" />
+		</div>
+	);
 }
 
 export default WaveEffect;
