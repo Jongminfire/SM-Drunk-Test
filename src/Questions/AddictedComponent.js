6dd8ca7c94999edaea9d5ccdda972ad7c20402b8
Jongminfire <51112542+Jongminfire@users.noreply.github.com>
2021-05-14 02:19:54 +0900

    feat: questions 모바일 UI 수정

--- src/Questions/AddictedComponent.js
+++ src/Questions/AddictedComponent.js
@@ -23,15 +23,6 @@ const QuestionContent = styled.div`
 	padding-top: 4vh;
 `;
 
-const QuestionMobile = styled.div`
-	background-color: #d4f3f6;
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	width: 100%;
-	height: 50vh;
-`;
-
 const InputForm = styled.input`
 	background-color: #ffffff;
 	border-radius: 10px;
@@ -80,6 +71,82 @@ const NextButton = styled.button`
 	font-size: 1.5rem;
 `;
 
+const QuestionMobile = styled.div`
+	background-color: #d4f3f6;
+	display: flex;
+	flex-direction: column;
+	position: absolute;
+	right: 5vw;
+	top: 5vh;
+	justify-content: center;
+	align-items: center;
+	width: 90vw;
+	height: 75vh;
+	border-radius: 2rem;
+`;
+
+const QuestionContentMobile = styled.div`
+	color: #126e82;
+	font-size: 1.5rem;
+	padding: 7vh 7vw 0;
+`;
+
+const QuestionNumberMobile = styled.div`
+	color: #126e82;
+	font-size: 3rem;
+	position: absolute;
+	top: 3vh;
+	left: 5vw;
+`;
+
+const InputFormMobile = styled.input`
+	background-color: #ffffff;
+	border-radius: 10px;
+	border: none;
+	outline: none;
+	height: 9vh;
+	width: 50vw;
+	font-size: 1.5rem;
+	color: #126e82;
+	padding-left: 1rem;
+`;
+
+const NextButtonMobile = styled.button`
+	border-radius: 5px;
+	background-color: #d8e3e7;
+	padding: 1vh 2vw;
+	border: none;
+	outline: none;
+	cursor: pointer;
+	position: absolute;
+	right: 8vw;
+	bottom: 5vh;
+	color: #126e82;
+	font-size: 1.5rem;
+`;
+
+const ButtonFormMobile = styled.button`
+	background-color: #ffffff;
+	border-radius: 0.5rem;
+	border: none;
+	outline: none;
+	height: 5vh;
+	min-width: 70vw;
+	font-size: 1rem;
+	cursor: pointer;
+	color: #126e82;
+	margin: 1vh 0 1vh;
+
+	&:hover {
+		transform: scale(1.1, 1.1);
+		transition: all ease-in-out 0.2s;
+	}
+
+	&:active {
+		transform: scale(0.85, 0.85);
+	}
+`;
+
 const AddictedComponent = (props) => {
 	const { score, setScore, setIndex, changeScore, isMobile, count, increaseIndex, setAnswers, setCards, data, addQnaData } = props;
 	const question = data.question;
@@ -99,20 +166,18 @@ const AddictedComponent = (props) => {
 	return (
 		<div>
 			{isMobile ? (
-				<Question>
-					<QuestionContainer>
-						<QuestionNumber>Q{count}.</QuestionNumber>
-						<QuestionContent>{question}</QuestionContent>
-						<div style={{ margin: "12vh 0" }} />
-						{answer.map((v) => {
-							return (
-								<ButtonForm key={v.text} onClick={() => selectAnswer(v.text, v.score)}>
-									{v.text}
-								</ButtonForm>
-							);
-						})}
-					</QuestionContainer>
-				</Question>
+				<QuestionMobile>
+					<QuestionNumberMobile>Q{count}.</QuestionNumberMobile>
+					<QuestionContentMobile>{question}</QuestionContentMobile>
+					<div style={{ margin: "2vh 0 3vh" }} />
+					{answer.map((v) => {
+						return (
+							<ButtonFormMobile key={v.text} onClick={() => selectAnswer(v.text, v.score)}>
+								{v.text}
+							</ButtonFormMobile>
+						);
+					})}
+				</QuestionMobile>
 			) : (
 				<Question>
 					<QuestionContainer>
